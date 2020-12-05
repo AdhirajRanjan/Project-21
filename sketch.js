09c0d224b7413d58b1e6a264fed03ad845660188
AdhirajRanjan <71758327+AdhirajRanjan@users.noreply.github.com>
2020-12-05 12:02:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,53 @@
 
 var car, wall;
 var speed, weight;
+var speed1, speed2, speed3;
+var weight1, weigth2, weight3;
 
 function setup() 
 {
-  createCanvas(1600,400);
+  createCanvas(1000,1000);
 
-  speed = random(55,90);
-  weight = random(400,1500);
-  car = createSprite(50,200,50,50);
-  car.velocityX = speed;
-  wall = createSprite(1500,200,60,height/2);
-  wall.shapeColor=color(80,80,80); 
+  speed1 = random(55,90);
+  speed2 = random(55,90);
+  speed3 = random(55,90);
+
+  weight1 = random(400,1500);
+  weight2 = random(400,1500);
+  weight3 = random(400,1500);
+
+  car1 = createSprite(50,200,50,50);
+  car1.velocityX = speed1;
+  car1.weight = weight1
+
+  wall1 = createSprite(900,200,60,200);
+  wall1.shapeColor=color(80,80,80); 
+  
+  car2 = createSprite(50,500,50,50);
+  car2.velocityX = speed2;
+  car2.weight = weight2
+
+  wall2 = createSprite(900,500,60,200);
+  wall2.shapeColor=color(80,80,80); 
+  
+  car3 = createSprite(50,800,50,50);
+  car3.velocityX = speed3;
+  car3.weight = weight3
+
+  wall3 = createSprite(900,800,60,200);
+  wall3.shapeColor=color(80,80,80); 
 }
 
 function draw() 
 {
   background(255,255,255);  
-  carCollide();
+  carCollide(wall1,car1,weight1,speed1);
+  carCollide(wall2,car2,weight2,speed2);
+  carCollide(wall3,car3,weight3,speed3);
   drawSprites();
 }
 
-function carCollide()
+function carCollide(wall,car,weight,speed)
 {
   if (wall.x-car.x < (car.width+wall.width)/2)
   {
@@ -29,17 +55,17 @@ function carCollide()
     var deformation = 0.5 * weight * speed * speed/22509;
     if (deformation>180)
     {
-      car.shapeColor=color(255,0,0);
+      wall.shapeColor=color(255,0,0);
     }
 
     if (deformation<180 && deformation>100)
     {
-      car.shapeColor=color(230,230,0);
+      wall.shapeColor=color(230,230,0);
     }
 
     if (deformation<100)
     {
-      car.shapeColor=color(0,255,0);
+      wall.shapeColor=color(0,255,0);
     }
   }
 }
